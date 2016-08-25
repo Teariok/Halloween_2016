56d470eb6fcf0a1e1fa55534c7a91e47b1229351
James Carr <teario@gmail.com>
2016-08-25 23:47:25 +0100

    Moved debug scripts into debug folder

---
+++ Unity/Assets/Game/Code/Debug/ColliderGizmo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Teario.Util
+{
+    public class ColliderGizmo : MonoBehaviour
+    {
+        void OnDrawGizmos()
+        {
+            BoxCollider lCollider = GetComponent<BoxCollider>();
+            if( lCollider )
+            {
+                Color lColour = Gizmos.color;
+                Gizmos.color = Color.green;
+
+                Gizmos.DrawWireCube( gameObject.transform.position + lCollider.center, lCollider.size );
+
+                Gizmos.color = lColour;
+            }
+        }
+    }
+}
