07e0c0504d7fab97512563611a87d7aea76b6ca7
James Carr <teario@gmail.com>
2016-08-25 22:24:27 +0100

    Enemy will now run if its speed is over a configurable threshold

--- Unity/Assets/Game/Code/EnemyStates/EnemyStateAttackPlayer.cs
+++ Unity/Assets/Game/Code/EnemyStates/EnemyStateAttackPlayer.cs
@@ -8,22 +8,36 @@ namespace Teario.Halloween
         private const string ANIMATION_NAME = "SwingNormal";
 
         private NavMeshObstacle m_NavObstacle;
+        private GameObject m_AttackCollider;
 
         public override void EnterState()
         {
             m_NavObstacle.enabled = true;
 
+            m_RootTransform.LookAt( Vector3.zero );
+            Quaternion lRot = m_RootTransform.rotation;
+            lRot.z = 0f;
+            m_RootTransform.rotation = lRot;
+
             PlayAnimation( ANIMATION_NAME );
+
+            m_AttackCollider.SetActive( true );
         }
         
         public override void ExitState()
         {
             m_NavObstacle.enabled = false;
+            m_AttackCollider.SetActive( false );
         }
     
         public void SetNavigationObstacle( NavMeshObstacle lNavObstacle )
         {
             m_NavObstacle = lNavObstacle;
+        }
+
+        public void SetAttackCollider( GameObject lObject )
+        {
+            m_AttackCollider = lObject;
         }
     }
 }
--- Unity/Assets/Game/Code/EnemyStates/EnemyStateSeekPlayer.cs
+++ Unity/Assets/Game/Code/EnemyStates/EnemyStateSeekPlayer.cs
@@ -11,15 +11,19 @@ namespace Teario.Halloween
         private float m_AttackRadius;
 
         private NavMeshAgent m_NavAgent;
+        private float m_Speed;
     
-        private const string ANIMATION_NAME = "Walk01";
+        private const string ANIMATION_WALK_NAME = "Walk01";
+        private const string ANIMATION_RUN_NAME = "Run";
+        private const float RUN_SWITCH_SPEED = 5f;
     
         public override void EnterState()
         {
             m_NavAgent.enabled = true;
             m_NavAgent.destination = Vector3.zero;
+            m_NavAgent.speed = m_Speed;
 
-            PlayAnimation( ANIMATION_NAME );
+            PlayAnimation( m_Speed < RUN_SWITCH_SPEED ? ANIMATION_WALK_NAME : ANIMATION_RUN_NAME );
             StartCoroutine( WaitForPlayerInRange( ()=>{
                 m_StateExitCallback( typeof(EnemyStateAttackPlayer) );
             } ) );
@@ -33,6 +37,11 @@ namespace Teario.Halloween
         public void SetNavigationAgent( NavMeshAgent lAgent )
         {
             m_NavAgent = lAgent;        
+        }
+
+        public void SetMoveSpeed( float lSpeed )
+        {
+            m_Speed = lSpeed;
         }
 
         private IEnumerator WaitForPlayerInRange( System.Action lCallback )
