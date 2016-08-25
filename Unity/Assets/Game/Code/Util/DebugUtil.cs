9970041707d60fc6e326f37017e08e7ee132fae5
James Carr <teario@gmail.com>
2016-08-25 23:04:07 +0100

    Added DebugUtils class to do logging
    This class can have its functions conditionally compiled out for release builds

---
+++ Unity/Assets/Game/Code/Util/DebugUtil.cs
@@ -0,0 +1,56 @@
+﻿#if UNITY_EDITOR || DEVELOPMENT_BUILD
+#define ENABLE_DEBUG_LOGS
+#endif
+
+using UnityEngine;
+using System.Diagnostics;
+
+using Debug = UnityEngine.Debug;
+
+namespace Teario.Util
+{
+    public class DebugUtil
+    {
+        [Conditional("ENABLE_DEBUG_LOGS")]
+        public static void Assert( bool lCondition )
+        {
+            Debug.Assert( lCondition );
+        }
+
+        [Conditional("ENABLE_DEBUG_LOGS")]
+        public static void Assert( bool lCondition, string lMessage )
+        {
+            Debug.Assert( lCondition, lMessage );
+        }
+
+        [Conditional("ENABLE_DEBUG_LOGS")]
+        public static void Log( object lMessage, Object lContext )
+        {
+            Debug.Log( lMessage, lContext );
+        }
+
+        [Conditional("ENABLE_DEBUG_LOGS")]
+        public static void LogError( object lMessage)
+        {
+            Debug.LogError( lMessage );
+        }
+
+        [Conditional("ENABLE_DEBUG_LOGS")]
+        public static void LogError( object lMessage, Object lContext )
+        {
+            Debug.LogError( lMessage, lContext );
+        }
+
+        [Conditional("ENABLE_DEBUG_LOGS")]
+        public static void LogError( System.Exception lException )
+        {
+            Debug.LogException( lException );
+        }
+
+        [Conditional("ENABLE_DEBUG_LOGS")]
+        public static void LogError( System.Exception lException, Object lContext )
+        {
+            Debug.LogException( lException, lContext );
+        }
+    }
+}
