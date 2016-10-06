ac445d798e50e7037c8473aa77e3c705bfa85135
James Carr <teario@gmail.com>
2016-10-06 23:35:25 +0100

    Used a few textures from the asset store to make the ui backgrounds better

--- Unity/Assets/Game/Code/UI/GameOverMenu.cs
+++ Unity/Assets/Game/Code/UI/GameOverMenu.cs
@@ -7,38 +7,16 @@ namespace Teario.Halloween
 {
     public class GameOverMenu : BaseMenu
     {
-        [SerializeField]
-        private Image m_Background;
-        [SerializeField]
-        private float m_FadeTime;
-
-        private float m_BackgroundTimer;
-        private Color m_BackgroundColour;
-
         void Start()
         {
-            m_BackgroundColour = m_Background.color;
         }
 
         public override void OnPreEnter()
         {
-            m_BackgroundTimer = 0f;
-            m_BackgroundColour.a = 0f;
-
-            m_Background.color = m_BackgroundColour;
         }
 
         void Update()
         {
-            if( m_BackgroundTimer < m_FadeTime )
-            {
-                if( (m_BackgroundTimer += Time.deltaTime) >= m_FadeTime )
-                {
-                }
-
-                m_BackgroundColour.a = Mathf.Clamp( m_BackgroundTimer / m_FadeTime, 0f, 1f );
-                m_Background.color = m_BackgroundColour;
-            }
         }
     }
 }
