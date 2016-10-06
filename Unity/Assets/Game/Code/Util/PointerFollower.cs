af521382395eb73671e6cd8417e57b1b016a0334
James Carr <teario@gmail.com>
2016-10-06 23:28:42 +0100

    Moved pointer follower to proper location

---
+++ Unity/Assets/Game/Code/Util/PointerFollower.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Teario.Util
+{
+    public class PointerFollower : MonoBehaviour
+    {
+        [SerializeField]
+        private Image m_Image;
+    
+        private BaseInput m_Controller;
+    
+        void Start()
+        {
+            m_Image.enabled = false;
+        }
+    
+    	void Update()
+        {
+            if( m_Controller != null )
+            {
+                Vector2 pos = m_Controller.GetCursorPosition();
+                m_Image.transform.position = new Vector3(pos.x, pos.y,0);
+            }
+    	}
+
+        public void SetController( BaseInput lController )
+        {
+            m_Controller = lController;
+            m_Image.enabled = lController != null;
+        }
+    }
+}
