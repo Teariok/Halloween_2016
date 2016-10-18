380591f5e18e94e2a79391a852d743dfc02d98ce
James Carr <teario@gmail.com>
2016-10-18 23:16:08 +0100

    Servo now opens the door, waits until the input buffer is empty then closes the door

--- Arduino/DispenserController.ino
+++ Arduino/DispenserController.ino
@@ -1,20 +1,38 @@
-const int OUT_PIN = 13;
+#include <Servo.h>
+
+Servo m_Servo;
+
+const int SERVO_PIN = 8;
+const int SERIAL_BAUD = 9600;
+
+const int SERVO_OPEN_VAL = 180;
+const int SERVO_CLOSED_VAL = 0;
+const int TRANSITION_DELAY_TIME = 400;
+const int OPEN_HOLD_TIME = 100;
 
 void setup()
 {
-	Serial.begin( 9600 );
-	pinMode( OUT_PIN, OUTPUT );
+	m_Servo.attach( SERVO_PIN );
+	m_Servo.write( SERVO_CLOSED_VAL );
+	
+	Serial.begin( SERIAL_BAUD );
 }
 
 void loop()
 {
 	if( Serial.available() )
 	{
-		int data = Serial.read();
-		Serial.println(data);
-		digitalWrite( OUT_PIN, HIGH );
-		delay( 100 );
-		digitalWrite( OUT_PIN, LOW );
-		delay( 100 );
+		m_Servo.write( SERVO_OPEN_VAL );
+		delay( TRANSITION_DELAY_TIME );
+		
+		while( Serial.available() )
+		{
+			delay( OPEN_HOLD_TIME );
+			int data = Serial.read();
+			Serial.println( data );
+		}
+		
+		m_Servo.write( SERVO_CLOSED_VAL );
+		delay( TRANSITION_DELAY_TIME );
 	}
 }
