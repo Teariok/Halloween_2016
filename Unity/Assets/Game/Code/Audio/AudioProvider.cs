14b289218ce4348d70650a414872797be8013cc0
James Carr <teario@gmail.com>
2016-11-09 23:52:50 +0000

    Moved some audio classes into a folder together. Moved some input classes into a folder together. Moved some util classes into the utils folder.

---
+++ Unity/Assets/Game/Code/Audio/AudioProvider.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Teario.Util
+{
+    public class AudioProvider : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioCollection[] m_Collections;
+    
+        public AudioClip GetRandom( string lCollectionName )
+        {
+            AudioCollection lCollection = FindCollection( lCollectionName );
+            Debug.Assert( lCollection != null );
+    
+            return lCollection.GetRandom();
+        }
+    
+        public AudioClip GetIndex( string lCollectionName, int lIndex )
+        {
+            AudioCollection lCollection = FindCollection( lCollectionName );
+            Debug.Assert( lCollection != null );
+    
+            return lCollection.GetIndex( lIndex );
+        }
+    
+        private AudioCollection FindCollection( string lName )
+        {
+            for( int i = 0; i < m_Collections.Length; ++i )
+            {
+                if( m_Collections[i].GetName().Equals( lName ) )
+                {
+                    return m_Collections[i];
+                }
+            }
+    
+            return null;
+        }
+    }
+}
