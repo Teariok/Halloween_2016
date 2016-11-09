e9e5a30834766b10875983fd53b61ba08c8d5922
James Carr <teario@gmail.com>
2016-11-09 23:46:37 +0000

    Put GenericRotator in util namespace

--- Unity/Assets/Game/Code/Util/GenericRotator.cs
+++ Unity/Assets/Game/Code/Util/GenericRotator.cs
@@ -1,24 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GenericRotator : MonoBehaviour
+namespace Teario.Util
 {
-    [SerializeField]
-    private Vector3 m_Speed;
-
-    private Vector3 m_Rotation;
-
-    void Start()
+    public class GenericRotator : MonoBehaviour
     {
-        m_Rotation = transform.localRotation.eulerAngles;
+        [SerializeField]
+        private Vector3 m_Speed;
+    
+        private Vector3 m_Rotation;
+    
+        void Start()
+        {
+            m_Rotation = transform.localRotation.eulerAngles;
+        }
+    
+    	void Update()
+        {
+            m_Rotation.x += m_Speed.x * Time.deltaTime;
+            m_Rotation.y += m_Speed.y * Time.deltaTime;
+            m_Rotation.z += m_Speed.z * Time.deltaTime;
+    
+            transform.localRotation = Quaternion.Euler( m_Rotation );
+    	}
     }
-
-	void Update()
-    {
-        m_Rotation.x += m_Speed.x * Time.deltaTime;
-        m_Rotation.y += m_Speed.y * Time.deltaTime;
-        m_Rotation.z += m_Speed.z * Time.deltaTime;
-
-        transform.localRotation = Quaternion.Euler( m_Rotation );
-	}
 }
